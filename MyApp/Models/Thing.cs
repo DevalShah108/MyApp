0513d20d6584dbf552bad202e3acfd99000ec565
deval <devals@sodelsolutions.com>
2013-06-18 13:49:48 +0530

    deployment test project

---
+++ MyApp/Models/Thing.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApp.Models 
+{
+    public class MyAppContext : DbContext
+    {
+        public DbSet<Thing> Things { get; set; }
+        public MyAppContext()
+            : base("MyAppContext")
+        {
+        }
+    }
+
+    public class Thing
+    {
+        public long ThingId { get; set; }
+        public string Name { get; set; }
+    }
+}
